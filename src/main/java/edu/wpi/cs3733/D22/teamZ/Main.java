f6cd38357569ddbbd954fdad10d7180cc5c46802
nadiaz2 <nadiaz2@wpi.edu>
2022-04-14 20:58:06 -0400

    Dev Start

--- src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.cs3733.D22.teamZ;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hi");
+    System.out.println("Hi ");
     App.launch(App.class, args);
   }
 }
