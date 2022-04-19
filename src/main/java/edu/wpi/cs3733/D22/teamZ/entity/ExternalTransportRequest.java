26a08be2ed4e009d5be45bc0d25236c78eb4245c
nadiaz2 <nadiaz2@wpi.edu>
2022-04-18 22:47:07 -0400

    Edit ExternalTransportRequest.java to use more lombok

--- src/main/java/edu/wpi/cs3733/D22/teamZ/entity/ExternalTransportRequest.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/entity/ExternalTransportRequest.java
@@ -1,13 +1,13 @@
 package edu.wpi.cs3733.D22.teamZ.entity;
 
 import java.time.LocalDate;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
+@RequiredArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class ExternalTransportRequest {
-  @NonNull private final String requestID;
+  @EqualsAndHashCode.Include @NonNull private final String requestID;
   @NonNull @Setter private RequestStatus status;
   @NonNull private final String issuerID;
   @NonNull @Setter private String handlerID;
@@ -15,33 +15,4 @@ public class ExternalTransportRequest {
   @NonNull @Setter private String transportDestination;
   @NonNull @Setter private LocalDate departureDate;
   @NonNull @Setter private TransportMethod transportMethod;
-
-  public ExternalTransportRequest(
-      @NonNull String requestID,
-      @NonNull RequestStatus status,
-      @NonNull String issuerID,
-      @NonNull String handlerID,
-      @NonNull String patientID,
-      @NonNull String transportDestination,
-      @NonNull LocalDate departureDate,
-      @NonNull TransportMethod transportMethod) {
-    this.requestID = requestID;
-    this.status = status;
-    this.issuerID = issuerID;
-    this.handlerID = handlerID;
-    this.patientID = patientID;
-    this.transportDestination = transportDestination;
-    this.departureDate = departureDate;
-    this.transportMethod = transportMethod;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o instanceof ExternalTransportRequest) {
-      ExternalTransportRequest objectCast = (ExternalTransportRequest) o;
-      return requestID.equals(objectCast.requestID);
-    } else {
-      return false;
-    }
-  }
 }
